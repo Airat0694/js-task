caa11fe0bcdd43868977bdbe3dbc1b3bc34ea21f
Airat0694 <moloko.94@bk.ru>
2017-08-21 18:23:20 +0400

    init dev

--- app/_include/js/script.js
+++ app/_include/js/script.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
-  // При загрузке страницы выполнится весь написанный здесь код
-  // alert("Привет!!!");
+  // for (var i = 0; i < 9; i++) {
+  //   $("#grid").append("<span></span>");
+  // }
+  // alert('sssss');
+
+  function draw(i) {
+    var y = (i < 3) ? 0 : (i < 6) ? 120 : 240;
+    var x = 120 * (i % 3);
+    $("#grid").append('<div class="square square' + i +
+      '"style="left:' + x + 'px; top:' + y + 'px;"></div>'
+    );
+  }
+
+  for (var i = 0; i < 9; i++) {
+    draw(i);
+  }
+
 });
